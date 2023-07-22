af37e5b069e6a38e69b2b5ca3dc76258a43b34ae
Israt07 <isratjahan335599@gmail.com>
2023-07-22 16:08:52 +0800

    Initial Commit

---
+++ TCPSummationServerApp/src/controller/NumberCalculator.java
@@ -0,0 +1,35 @@
+package controller;
+
+/**
+ * 
+ * @Israt Jahan Bhuiyan
+ *
+ */
+
+public class NumberCalculator {
+
+    /**
+     * A method that will get the summation from three numbers
+     * 
+     * @param number1
+     * @param number2
+     * @param number3
+     * @return
+     */
+    public int getSum(int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+    }
+
+    /**
+     * A method that will get the multiplication from three numbers
+     * 
+     * @param number1
+     * @param number2
+     * @param number3
+     * @return
+     */
+    public int getMultiplication(int number1, int number2, int number3) {
+        return number1 * number2 * number3;
+    }
+
+}
